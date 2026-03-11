c0c5c5942a188554963b6ece51a310aaa15a7c1c
ekram-haque <dev.ekramul@gmail.com>
2026-03-12 02:45:41 +0600

    display words by dynamic id

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ const displayLesson = (lessons) => {
     const lessonBtn = document.createElement("div");
     // add dynamic button for every lesson
     lessonBtn.innerHTML = `
-         <button class="btn btn-outline btn-primary"
+         <button onClick="loadLessonWord(${lesson.level_no})" class="btn btn-outline btn-primary"
             ><i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}</button
           >
         
@@ -27,5 +27,45 @@ const displayLesson = (lessons) => {
     lessonsContainer.appendChild(lessonBtn);
   }
 };
-
+// load lesson on open the site
 loadLesson();
+
+const loadLessonWord = (id) => {
+  const url = `https://openapi.programming-hero.com/api/level/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((levelWord) => displayWord(levelWord.data));
+};
+
+// word object schema
+// {
+//   "id": 4,
+//   "level": 5,
+//   "word": "Diligent",
+//   "meaning": "পরিশ্রমী",
+//   "pronunciation": "ডিলিজেন্ট"
+// }
+
+const displayWord = (words) => {
+  const wordsContainer = document.getElementById("wordsContainer");
+  wordsContainer.innerText = "";
+
+  words.forEach((word) => {
+    const wordDiv = document.createElement("div");
+    wordDiv.innerHTML = `
+                <div class=" py-10 px-5 bg-gray-50 space-y-3 m-3 shadow-sm rounded-lg">
+            <h2 class="text-3xl font-bold">${word.word}</h2>
+           <div>
+            <p class="font-semibold">Meaning / Pronounciation</p>
+            <p class="font-semibold">"${word.meaning} / ${word.pronunciation}"</p>
+           </div>
+            <div class="flex justify-between mt-3">
+                <button class="btn  bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+                <button class="btn  bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+            </div>
+        </div>
+        
+        `;
+    wordsContainer.append(wordDiv);
+  });
+};
